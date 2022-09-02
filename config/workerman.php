263830889c2c6d6b2e328eee4dc6e39db4427c52
mouyong <mouyong@users.noreply.github.com>
2022-09-02 15:36:38 +0800

    Update workerman.php

--- config/workerman.php
+++ config/workerman.php
@@ -3,6 +3,7 @@
 return [
     'memory_limit' => '512M',
 
+    // all: https://github.com/walkor/workerman/tree/master/src/Events
     // swoole: \Workerman\Events\Swoole:class
     'eventLoopClass' => null,
 
